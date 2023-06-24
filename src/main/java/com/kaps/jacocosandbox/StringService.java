5743911ed7ab0ef6e925e0cb969ac268ec1a698c
mehul-kaps <49353327+mehul-kaps@users.noreply.github.com>
2023-06-24 12:54:17 -0400

    Update StringService.java

--- src/main/java/com/kaps/jacocosandbox/StringService.java
+++ src/main/java/com/kaps/jacocosandbox/StringService.java
@@ -1,12 +1,11 @@
 package com.kaps.jacocosandbox;
 
 /**
- * 
  * @author mehulkapadia
- *
  */
 public class StringService {
 
+	public Integer counter = 9;
 	public static String getValue() {
 		return "Hello";
 	}
